c68595db22f13b8693e0fd918338d0343e10a545
sarahkhaldi <79889859+sarahkhaldi@users.noreply.github.com>
2023-05-04 22:43:52 -0400

    Add files via upload

--- recursion.cpp
+++ recursion.cpp
@@ -17,8 +17,24 @@ void printRange(int left, int right)
  }
 }
 
+int sumRange(int left, int right)
+{
+ int sum = 0;
+ if (left <= right)
+ {
+  sum = left + sumRange(left + 1, right);
+ }
+ return sum;
+}
+
 int main()
 {
- printRange(-2, 10);
+ // printRange(-2, 10);
+
+ int x = sumRange(1, 3);
+ cout << "This is " << x << endl; // 6
+
+ int y = sumRange(-2, 10);
+ cout << "That is " << y << endl; // 52
  return 0;
 }
